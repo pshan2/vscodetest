dd9486c2e08090b9d8fa2712cbec8bae3a56bb2f
Pengyin Shan <pshan2@emory.edu>
2016-06-21 17:03:03 -0400

    change

---
+++ test.js
@@ -0,0 +1,4 @@
+var fullName = "Bob";
+var age = 37;
+var sentence = "Hello, my name is " + fullName + ".\n\n I'll be " + (age + 1) + " years old next month.";
+var sentence2 = "Hello, my name is " + fullName + ".\n\n" + "I'll be " + (age + 1) + " years old next month.";
---
+++ test.ts
@@ -0,0 +1,6 @@
+let fullName: string = `Bob`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullName}.
+
+ I'll be ${age+1} years old next month.`
+let sentence2: string = "Hello, my name is " + fullName + ".\n\n" + "I'll be " + (age+1) + " years old next month."                            
